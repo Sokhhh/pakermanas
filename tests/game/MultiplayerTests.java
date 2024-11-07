55c03330a5a04f9a7041b4cf4fce7f8bdf848e69
Tomas Sakalauskas <tomass.sakalauskas@gmail.com>
2024-11-07 23:07:39 +0200

    tests branch initial

---
+++ tests/game/MultiplayerTests.java
@@ -0,0 +1,12 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MultiplayerTests {
+    @Test
+    public void testExample() {
+        assertEquals(2, 1 + 1);  // Simple test to verify JUnit setup
+    }
+}
